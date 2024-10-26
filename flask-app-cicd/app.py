94d13b85222864ae66e047bf7f87933c78228c60
DiametrFQ <hohlov.03@inbox.ru>
2024-10-26 22:07:08 +0300

    change ci/cd

--- flask-app-cicd/app.py
+++ flask-app-cicd/app.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,4 +14,21 @@ def data():
     return 'This is some data!'
 
 if __name__ == '__main__':
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+# Маршрут для главной страницы
+@app.route('/')
+def home():
+    return 'Welcome to the home page!'
+
+# Маршрут для страницы данных
+@app.route('/data')
+def data():
+    return 'This is some data!'
+
+if __name__ == '__main__':
+>>>>>>> Stashed changes
     app.run(host='0.0.0.0', port=5000),
