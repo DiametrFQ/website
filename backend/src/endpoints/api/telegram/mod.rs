5464088ffe174004f5e6f510cb333dcce1103675
Dmitry Khokhlov <dmitry.khokhlov@cyberia-tech.com>
2025-10-02 15:20:41 +0300

    Ref(backend): Switch backend on TypeScript

--- backend/src/endpoints/api/telegram/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod handlers;
-pub mod models;
-pub mod routes;
-pub mod services;
-pub use super::common::errors;
--- backend/src/endpoints/api/telegram/routes.rs
+++
@@ -1,6 +0,0 @@
-use super::handlers;
-use actix_web::web;
-
-pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/telegram").service(handlers::get_telegram_posts_handler));
-}
